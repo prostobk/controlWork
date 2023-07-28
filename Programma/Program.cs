ac279e374d8a7dcd2a4f8dce32841703e2610d73
Nikolay <prostobk18@gmail.com>
2023-07-28 17:52:56 +0300

    Внесены косметические корректировки в код

--- Programma/Program.cs
+++ Programma/Program.cs
@@ -1,27 +1,27 @@
-﻿string[] basearray = new string[10] {"один", "1", "два", "2", "три", "3", "четыре", "4", "пять", "5"};
-string[] newarray = new string[basearray.Length];
+﻿string[] baseArray = new string[10] {"один", "1", "два", "2", "три", "3", "четыре", "4", "пять", "5"};
+string[] newArray = new string[baseArray.Length];
 
-void GetNewArray(string[] basearray, string[] newarray)
+void GetNewArray(string[] baseArray, string[] newArray)
 {
     int count = 0;
-    for (int i = 0; i < basearray.Length; i++)
+    for (int i = 0; i < baseArray.Length; i++)
     {
-        if (basearray[i].Length <=3)
+        if (baseArray[i].Length <=3)
         {
-            newarray[count] = basearray[i];
+            newArray[count] = baseArray[i];
             count++;
         }
     }
 }
 
-void PrintNewArray(string[] newarray)
+void PrintNewArray(string[] newArray)
 {
-    for (int i = 0; i < newarray.Length; i++)
+    for (int i = 0; i < newArray.Length; i++)
     {
-        Console.Write($"{newarray[i]} ");
+        Console.Write($"{newArray[i]} ");
     }
     Console.WriteLine();
 }
 
-GetNewArray(basearray, newarray);
-PrintNewArray(newarray);
+GetNewArray(baseArray, newArray);
+PrintNewArray(newArray);
